928aad860e1765c160301cd6084242c66d9c15fc
Oleg <olegkhristenko@gmail.com>
2021-03-12 09:29:41 +0200

    Started html CV

--- src/components/Main/Main.vue
+++ src/components/Main/Main.vue
@@ -1,15 +1,106 @@
 <template>
     <div>
-        <h2>Main</h2>
+        <div class="cv container">
+            <div class="cv__left">
+                <div class="cv__left-block">
+                    <div class="cv__left-block-img">
+                        <img src="@/assets/images/ava.png" alt="ava">
+                    </div>
+                    <h2 class="cv__left-block-name">Oleg Khristenko</h2>
+                    <p class="cv__left-block-profession">Front-End Developer</p>
+                </div>
+                <div class="cv__left-block">
+                    <h3 class="cv__left-block-title">Summary</h3>
+                    <p class="cv__left-block-text">Results-oriented software developer and research professional. I make it my
+                        goal to create software with the user in mind, creating applications with a useable and intuitive
+                        user interface experience. I also understand the importance of creating highly readable and easily
+                        maintainable source code. I am constantly striving to learn new technologies and look to ways to
+                        better myself in this rapidly changing industry.</p>
+                </div>
+                <div class="cv__left-block">
+                    <h3 class="cv__left-block-title">Tech skills</h3>
+                    <p class="cv__left-block-text">
+                        <span>-JavaScript</span>
+                        <span>-JQuery</span>
+                        <span>-Vue</span>
+                        <span>-React</span>
+                        <span>-AJAX</span>
+                        <span>-Git</span>
+                        <span>-WebPack</span>
+                        <span>-Gulp</span>
+                        <span>-HTML5</span>
+                        <span>-CSS3 (Sass, SCSS, Less, Grid, Flexbox)</span>
+                        <span>-Adaptive Web Design</span>
+                        <span>-Photoshop</span>
+                        <span>-English (Pre-Intermediate)</span>
+                    </p>
+                </div>
+                <div class="cv__left-block">
+                    <h3 class="cv__left-block-title">Soft skills</h3>
+                    <p class="cv__left-block-text">
+                        <span>-Resposible</span>
+                        <span>-Scrupulouse</span>
+                        <span>-Teamwork</span>
+                    </p>
+                </div>
+                <div class="cv__left-block">
+                    <h3 class="cv__left-block-title">Contacts</h3>
+                    <p class="cv__left-block-text">
+                        <span>Kyiv, Ukraine</span>
+                        <span>Telephone: <a href="tel:+380508899268">+380 50 88 99 268</a></span>
+                        <span>Telegram: <a href="tg://resolve?domain=@kholan">@kholan</a></span>
+                        <span>Skype: <a href="skype:hristenkoleg?chat">hristenkoleg</a></span>
+                        <span>Email: <a href="mailto:olegkhristenko@gmail.com">olegkhristenko@gmail.com</a></span>
+                        <span>GitHub: <a href="https://github.com/Kh-Ol-An">github.com/Kh-Ol-An</a></span>
+                        <span>LinkedIn: <a href="https://linkedin.com/in/kh-ol-an">linkedin.com/in/kh-ol-an</a></span>
+                        <span>Website: <a href="/">kholan.tech</a></span>
+                    </p>
+                </div>
+            </div>
+
+            <div class="cv__right">
+                <div class="cv__right-block">
+                    <h3 class="cv__right-title">Portfolio</h3>
+                    <p class="cv__right-text">
+                        <span>Smart Group</span>
+                        <a href="https://smartgroup.ua">https://smartgroup.ua/</a>
+                    </p>
+                    <p class="cv__right-text">
+                        <span>WO</span>
+                        <a href="https://wo.ua">https://wo.ua/</a>
+                    </p>
+                    <p class="cv__right-text">
+                        <span>Cecotec</span>
+                        <a href="https://cecotec.tech">https://cecotec.tech/</a>
+                    </p>
+                    <p class="cv__right-text">
+                        <span>Deerma</span>
+                        <a href="https://deerma.ua">https://deerma.ua/</a>
+                    </p>
+                    <p class="cv__right-text">
+                        <span>Profit Whales</span>
+                        <a href="https://profitwhales.com">https://profitwhales.com/</a>
+                    </p>
+                    <p class="cv__right-text">
+                        <span>Personal site</span>
+                        <a href="http://kholan.tech">http://kholan.tech/</a>
+                    </p>
+                </div>
+                <div class="cv__right-block">
+                    <h3 class="cv__right-block-title">Experience</h3>
+                    <div class="cv__right-block-wrap"></div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-name: "Main"
+    name: "Main"
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 
 </style>
